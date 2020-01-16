5938e9e38b5e60d45f32b7389f3312624cef081f
Nicolas Cabrera <nicolascc1@hotmail.com>
2020-01-16 11:56:47 +0000

    frontend almost done

--- src/Component/Accordion.js
+++ src/Component/Accordion.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 
 export default function Accordion(props) {
 
-	if(props.step === '1') {
+	const [active, setActive] = useState('notactive');
+	const [height, setHeight] = useState('48px');
+	const content = useRef(null);
+
+	function toggleActive(stepnumber) {
+		//console.log(stepnumber); 
+		console.log(props.step);
+		setActive(active === 'notactive' ? 'active' : 'notactive');
+		setHeight(active === 'active' ? '48px' : '200px')
+	}
+
+	if (props.step === '1') {
 		return (
-			<div>
-				<div className='form-section'>
+			<div className={`accordion ${active}`}>
+				<button onClick={() => toggleActive(1)} />
+				<div ref={content} style={{maxHeight: `${height}`}} className='form-section'>
 					<h3 className='step-section'>Step {props.step}: {props.section}</h3>
 					<form className='form'>
 						<div className='form-group'>
@@ -17,47 +29,47 @@ export default function Accordion(props) {
 						<div className='form-group-right'>
 							<label>{props.thirdlabel}</label>
 							<input type='text'></input>
-						</div>					
+						</div>
+						<button>Next </button>
 					</form>
-					<button>Next ></button>
 				</div>
 			</div>
 		)
-	} else if (props.step === '2'){
+	} else if (props.step === '2') {
 		return (
-			<div>
-				<div className='form-section'>
+			<div className={`accordion ${active}`}>
+				<button onClick={() => toggleActive(2)} />
+				<div ref={content} style={{maxHeight: `${height}`}} className='form-section'>
 					<h3 className='step-section'>Step {props.step}: {props.section}</h3>
 					<form className='form'>
 						<div className='form-group'>
 							<label>{props.firstlabel}</label>
 							<input type='text'></input>
 							<label>{props.secondlabel}</label>
-								<input className='dob' type='text' />
-								<input className='dob' type='text' />
-								<input className='dob' type='text' />
+							<input className='dob' type='text' />
+							<input className='dob' type='text' />
+							<input className='dob' type='text' />
 						</div>
 						<div className='form-group-right'>
 							<label>{props.thirdlabel}</label>
 							<select type='select'><option>Select Gender &nbsp;&nbsp;v</option></select>
-						</div>					
+						</div>
 					</form>
-					<button>Next ></button>
 				</div>
 			</div>
 		)
-	} else if(props.step === '3') {
+	} else if (props.step === '3') {
 		return (
-			<div>
-				<div className='form-section'>
+			<div className={`accordion ${active}`}>
+				<button onClick={() => toggleActive(3)} />
+				<div ref={content} style={{maxHeight: `${height}`}} className='form-section'>
 					<h3 className='step-section'>Step {props.step}: {props.section}</h3>
 					<form className='form'>
 						<div className='form-group'>
 							<label className=''>{props.firstlabel}</label>
 							<input className='comment-input' type='text'></input>
-						</div>				
+						</div>
 					</form>
-					<button className='last-button'>Next ></button>
 				</div>
 			</div>
 		)
